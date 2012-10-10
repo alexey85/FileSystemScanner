17ca6b9c60669f3bcc2e5c85bb6fe163180ba079
Alexey Govallo <alex.govallo@gmail.com>
2012-10-11 00:01:13 +0400

    dummy executing test + update ignores

---
+++ src/test/java/fs/test/MainTest.java
@@ -0,0 +1,14 @@
+package fs.test;
+
+import eg.Main;
+
+/**
+ * User: alexey
+ * Date: 10.10.12 23:44
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        String[] arg = {"src/test/testWorkDir/","3"};
+        Main.main(arg);
+    }
+}
